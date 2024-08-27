d614a25dcd27c9f354beeb1fa24e52e61e6db711
tarekwfa0110 <109884541+tarekwfa0110@users.noreply.github.com>
2024-08-27 13:38:09 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">Header</div>
+      <div className="medium">
+        <div className="nav">Nav</div>
+        <div className="main">Main</div>
+        <div className="sidebar">Sidebar</div>
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
